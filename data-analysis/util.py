fd0192f419de1d3de3274fc91df154ae47312312
alpgarcia <alpgarcia@gmail.com>
2017-05-04 17:50:42 +0200

    Add experience calculations

--- data-analysis/util.py
+++ data-analysis/util.py
@@ -2,6 +2,8 @@
 import certifi
 import configparser
 
+import pandas
+
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 
@@ -19,9 +21,23 @@ def ESConnection():
 
     connection = "https://" + user + ":" + password + "@" + host + ":" + port + "/" + path
 
-    es_read = Elasticsearch([connection], use_ssl=True, verity_certs=True, ca_cert=certifi.where(), scroll='300m')
+    es_read = Elasticsearch([connection], use_ssl=True, verity_certs=True, ca_cert=certifi.where(), scroll='300m',
+	timeout=100)
 
     return es_read
+
+
+def to_df_by_time(result, group_column, time_column, value_column, group_field, time_field, value_field):
+    """Creates a dataframe based on group and time values
+    """
+    df = pandas.DataFrame(columns=[group_column, time_column, value_column])
+
+    for time in result.to_dict()['aggregations'][time_field]['buckets']:
+        for group in time[group_field]['buckets']:
+            df.loc[len(df)] = [group['key'], time['key_as_string'], group[value_field]['value']]
+    
+    return df
+
 
 def test():
     es_conn = ESConnection()
