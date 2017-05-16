bfb214a942a59c4375894712c02aa22aafddf375
alpgarcia <alpgarcia@gmail.com>
2017-05-16 14:26:00 +0200

    Add projects from Mozilla spreadsheet

--- data-analysis/util.py
+++ data-analysis/util.py
@@ -2,7 +2,7 @@
 import certifi
 import configparser
 
-import pandas
+import pandas as pd
 
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
@@ -31,7 +31,7 @@ def to_df_by_time(result, group_column, time_column, value_column,subgroup_colum
 		 group_field, time_field, value_field, subgroup_field):
     """Creates a dataframe based on group and time values
     """
-    df = pandas.DataFrame(columns=[group_column, time_column, value_column, subgroup_column])
+    df = pd.DataFrame(columns=[group_column, time_column, value_column, subgroup_column])
 
     for time in result.to_dict()['aggregations'][time_field]['buckets']:
         for group in time[group_field]['buckets']:
@@ -40,6 +40,18 @@ def to_df_by_time(result, group_column, time_column, value_column,subgroup_colum
 
     return df
 
+
+def read_projects(filepath):
+    xl = pd.ExcelFile(filepath)
+    project_groups = {}
+    for sheet_name in xl.sheet_names:
+        project_groups[sheet_name] = xl.parse(sheet_name)
+
+    # FIX GITHUB REPO NAMES BY ADDING .git TO THE END
+
+    project_groups['Github']['Repo'] = project_groups['Github']['Repo'] + '.git'
+
+    return project_groups
 
 def test():
     es_conn = ESConnection()
@@ -51,5 +63,14 @@ def test():
         print(item)
         break
 
+def test_xls():
+    pg = read_projects("data/Contributors and Communities Analysis - Project grouping.xlsx")
+
+    for key in pg.keys():
+        print(key)
+
+    print(pg['Github'])
+
 if __name__ == "__main__":
     test()
+    test_xls()
