0847526ebe0d6f07de14d892acbbcd1513d1f081
alpgarcia <alpgarcia@gmail.com>
2017-05-19 13:58:57 +0200

    Split patterns and retention by projecSplit patterns and retention by project

---
+++ data-analysis/run-by-project.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+jupyter-runner --parameter-file=project_names --output-directory=project-specific Project-Attraction-Retention.ipynb
+
+jupyter-runner --parameter-file=project_names --output-directory=project-specific Understanding\ Contribution\ Patterns.ipynb
